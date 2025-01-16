bfa5802567b3c5f9d59e50bde802fa750802d7cb
evangelion08 <evacardelldevries@gmail.com>
2025-01-16 13:53:45 +0100

    keys

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ class Player {
         this.velocity = velocity
         this.radius = 15
     }
+
     draw() {
         c.beginPath()
         c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
@@ -137,6 +138,8 @@ addEventListener('keydown', ({ key }) => {
             lastKey = 'd'
             break
     }
+    console.log(keys.d.pressed)
+    console.log(keys.s.pressed)
 })
 
 addEventListener('keyup', ({ key }) => {
@@ -154,4 +157,6 @@ addEventListener('keyup', ({ key }) => {
             keys.d.pressed = false
             break
     }
+    console.log(keys.d.pressed)
+    console.log(keys.s.pressed)
 })
